3f87d50e721044f6366bf4db85f551934c35a01e
Patricia Naomi <132932532+patinaomi@users.noreply.github.com>
2024-07-01 11:45:43 -0300

    Update 005.cs

--- semana2/005.cs
+++ semana2/005.cs
@@ -10,26 +10,18 @@ namespace Exercicio
     {
         static void Main(string[] args)
         {
-            /*
-             * Escreva um programa que solicite ao usuário dois números e exiba a soma,
-             * subtração, multiplicação e divisão entre eles */
+            // Escreva um programa que calcule a média aritmética de dois números.
 
-            Console.WriteLine("Digite um número: ");
-            float n1 = float.Parse(Console.ReadLine());
-
-            Console.WriteLine("Digite o segundo número: ");
-            float n2 = float.Parse(Console.ReadLine());
-
-            float soma = n1 + n2;
-            float subtracao = n1 - n2;
-            float multiplicacao = n1 * n2;
-            float divisao = n1 / n2;
-
-            Console.WriteLine(" --- Resultado --- ");
-            Console.WriteLine($"Soma: {soma}");
-            Console.WriteLine($"Subtração: {subtracao}");
-            Console.WriteLine($"Multiplicação: {multiplicacao}");
-            Console.WriteLine($"Divisão: {divisao}");  
+             Console.WriteLine("-- Cálculo de Média --");
+             Console.WriteLine("Digite um número: ");
+             float n1 = float.Parse(Console.ReadLine());
+            
+             Console.WriteLine("Digite o segundo número: ");
+             float n2 = float.Parse(Console.ReadLine());
+            
+             float media = (n1 + n2) / 2;
+             Console.WriteLine("Média: " + media);
+             Console.WriteLine($"Média: {media}"); 
         }
     }
 }
