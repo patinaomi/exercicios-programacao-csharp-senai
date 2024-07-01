230ba631c5540801f8034962b574093b5fb46ed6
Patricia Naomi <132932532+patinaomi@users.noreply.github.com>
2024-07-01 11:44:55 -0300

    Update and rename 006.py to semana2/006.cs

--- 006.py
+++
@@ -1,8 +0,0 @@
-lista = ['goiaba', 'abacaxi', 'melancia', 'caju', 'kiwi']
-
-busca = input('Digite sua busca: ')
-
-if busca in lista:
-    print(f'O elemento {busca} se encontra na lista.')
-else:
-    print(f'O elemento {busca} não se encontra na lista.')
